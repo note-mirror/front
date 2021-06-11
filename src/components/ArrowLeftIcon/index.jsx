3b2c8be08c0f15c2ca62f8dc06c18d9c2bb8bfb4
unknown <jvitor.sche@gmail.com>
2021-06-11 18:26:42 -0300

    fixed bug

--- src/components/ArrowLeftIcon/index.jsx
+++ src/components/ArrowLeftIcon/index.jsx
@@ -1,5 +1,7 @@
-export default () => (
+const Icon = () => (
   <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
     <path fill="currentColor" d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" />
   </svg>
 )
+
+export default Icon
